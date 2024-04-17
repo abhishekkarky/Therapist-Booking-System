9a04a8fcd8c003a4316758d1bc56711c8b32790e
avi-13 <avicekbhatta.123@gmail.com>
2024-04-17 23:32:01 +0545

    testimonial

--- Home/views.py
+++ Home/views.py
@@ -2,8 +2,15 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+from Auth.models import Testimonials
 from Home.models import ContactList
 
 
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        testimonials = Testimonials.objects.all()
+        if request.user.is_admin:
+            messages.error(request, 'You donot have access to this page.')
+            return redirect('/admin-page')
+
+    return render(request, 'home.html', {"testimonials": testimonials})
